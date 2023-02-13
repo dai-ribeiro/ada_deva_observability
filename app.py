80074044f42d1ffd2514853ec1dfc7701bb64fb2
Daiane-WIN <dai_ribeiro@hotmail.com>
2023-02-13 13:50:07 -0300

    criacao de app.py, requirements.txt e templantes renda fixa

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/rendafixa')
+def renda_fixa():
+    return render_template('lista.html', titulo='Renda Fixa')
