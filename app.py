ffcdc6f2e51b3410c3f95555c6615f2808f8abd4
Daiane-WIN <dai_ribeiro@hotmail.com>
2023-02-27 19:17:47 -0300

    criacao do cliente-app

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
+import random
+import time
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
+import prometheus_client as prom
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+quantidade_usuarios_online = prom.Gauge('quantidade_usuarios_online', 
+                                        'Número de usuários online no momento')
+
+def parametros_endpoint():
+    time.sleep(random.randint(1, 10))
+    quantidade_usuarios_online.set(random.randint(1, 100))
+
 @app.route('/renda-fixa')
 def renda_fixa():
+    parametros_endpoint()
     return render_template('lista.html', titulo='Renda Fixa')
 
+@app.route('/renda-variavel')
+def renda_variavel():
+    parametros_endpoint()
+    return render_template('lista.html', titulo='Renda Variável')
+
+@app.route('/fii')
+def fii():
+    parametros_endpoint()
+    return render_template('lista.html', titulo='FIIs')
+
+@app.route('/cripto')
+def cripto():
+    parametros_endpoint()
+    return render_template('lista.html', titulo='Cripto')
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
