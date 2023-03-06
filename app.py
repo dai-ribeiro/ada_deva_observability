d820e98f8468937049c05977b0edeb834c930f44
Daiane-WIN <dai_ribeiro@hotmail.com>
2023-03-06 18:40:50 -0300

    alteracoes: docker-compose, app.py e grafana.db

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import http.client
+import logging
+import prometheus_client as prom
 import random
 import time
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
 from prometheus_flask_exporter import PrometheusMetrics
-import prometheus_client as prom
+from time import strftime
+
+# configure the logger
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -16,22 +26,34 @@ def parametros_endpoint():
 
 @app.route('/renda-fixa')
 def renda_fixa():
+    app.logger.info('%s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path)
     parametros_endpoint()
+    if random.randint(0,1) == 0:
+        return http.client.BAD_REQUEST
     return render_template('lista.html', titulo='Renda Fixa')
 
 @app.route('/renda-variavel')
 def renda_variavel():
+    app.logger.info('%s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path)
     parametros_endpoint()
+    if random.randint(0,1) == 0:
+        return http.client.BAD_REQUEST
     return render_template('lista.html', titulo='Renda Variável')
 
 @app.route('/fii')
 def fii():
     parametros_endpoint()
+    app.logger.info('%s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path)
+    if random.randint(0,1) == 0:
+        return http.client.BAD_REQUEST
     return render_template('lista.html', titulo='FIIs')
 
 @app.route('/cripto')
 def cripto():
+    app.logger.info('%s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path)
     parametros_endpoint()
+    if random.randint(0,1) == 0:
+        return http.client.BAD_REQUEST
     return render_template('lista.html', titulo='Cripto')
 
 
